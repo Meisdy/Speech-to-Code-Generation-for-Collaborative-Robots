7646571548f1e1c03c9fec7734893a005a029c41
BOTSpencer <sandy.meister@gmx.ch>
2026-02-10 16:49:10 +0100

    Other version that uses presaved programs on the robot and calls these. Timer in between is needed to not inerrupt something.

--- Testing/UR_Gripper_test.py
+++ Testing/UR_Gripper_test.py
@@ -3,42 +3,36 @@ import time
 
 ROBOT_IP = "169.254.70.80"
 
-# Connect to dashboard
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((ROBOT_IP, 29999))
+print("Starting gripper test...")
 
-# Power on
-sock.send(b"power on\n")
-print(sock.recv(1024).decode())
-time.sleep(2)
+try:
+    print("Opening new connection...")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ROBOT_IP, 29999))
+    sock.recv(1024)
 
-sock.send(b"brake release\n")
-print(sock.recv(1024).decode())
-time.sleep(2)
+    print("Loading program to open Gripper...")
+    sock.send(b"load /programs/open_UG2_Gripper.urp\n")
+    response = sock.recv(1024).decode()
+    print(f"Load response: {response}")
+    print("Playing program...")
+    sock.send(b"play\n")
+    response = sock.recv(1024).decode()
+    print(f"Play response: {response}")
 
-# Load a program (you need to have one saved)
-sock.send(b"load /programs/test.urp\n")  # Change to your program name
-print(sock.recv(1024).decode())
-time.sleep(1)
+    time.sleep(3)
 
-# Play the program
-sock.send(b"play\n")
-print(sock.recv(1024).decode())
-time.sleep(2)
+    print("Loading program to Close Gripper...")
+    sock.send(b"load /programs/close_UG2_Gripper.urp\n")
+    response = sock.recv(1024).decode()
+    print(f"Load response: {response}")
+    print("Playing program...")
+    sock.send(b"play\n")
+    response = sock.recv(1024).decode()
+    print(f"Play response: {response}")
 
-sock.close()
+    sock.close()
+    print("Done!")
 
-# NOW send gripper commands to port 30002
-print("\nSending gripper command...")
-script = """
-popup("Gripper test", title="Info", warning=False, error=False)
-RG6(target_width=80, target_force=20, payload=0.0, set_payload=False, depth_compensation=False, slave=False)
-"""
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((ROBOT_IP, 30002))
-sock.send(script.encode('utf-8'))
-time.sleep(5)
-sock.close()
-
-print("Done!")
+except Exception as e:
+    print(f"ERROR: {e}")
