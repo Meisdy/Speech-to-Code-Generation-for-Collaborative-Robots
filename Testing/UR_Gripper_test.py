ec2cbbfd9f1b02bc0978a4d875d942dbd4cf461f
BOTSpencer <sandy.meister@gmx.ch>
2026-02-10 14:24:04 +0100

    Finally gripper test worked using this code

--- Testing/UR_Gripper_test.py
+++ Testing/UR_Gripper_test.py
@@ -1,22 +1,44 @@
 import socket
 import time
 
-ROBOT_IP = "169.254.70.80"  # e.g., UR5e
-port = 30002
+ROBOT_IP = "169.254.70.80"
 
-# Test script
+# Connect to dashboard
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((ROBOT_IP, 29999))
+
+# Power on
+sock.send(b"power on\n")
+print(sock.recv(1024).decode())
+time.sleep(2)
+
+sock.send(b"brake release\n")
+print(sock.recv(1024).decode())
+time.sleep(2)
+
+# Load a program (you need to have one saved)
+sock.send(b"load /programs/test.urp\n")  # Change to your program name
+print(sock.recv(1024).decode())
+time.sleep(1)
+
+# Play the program
+sock.send(b"play\n")
+print(sock.recv(1024).decode())
+time.sleep(2)
+
+sock.close()
+
+# NOW send gripper commands to port 30002
+print("\nSending gripper command...")
 script = """
-def test_gripper():
-    rg_grip(110, 20)  # Open
-    sleep(2)
-    rg_grip(50, 40)   # Close
-    sleep(2)
-    rg_grip(110, 20)  # Open again
-end
-test_gripper()
+popup("Gripper test", title="Info", warning=False, error=False)
+RG6(target_width=80, target_force=20, payload=0.0, set_payload=False, depth_compensation=False, slave=False)
 """
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((ROBOT_IP, port))
-sock.send(script.encode())
+sock.connect((ROBOT_IP, 30002))
+sock.send(script.encode('utf-8'))
+time.sleep(5)
 sock.close()
+
+print("Done!")
