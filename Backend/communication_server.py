b3c9e00349008ff3b462d5102ce3fceed096573b
Sandy Meister <sandy.meister@gmx.ch>
2026-02-24 15:26:29 +0100

    Implemented a polling approach to fix the bugs

--- Backend/communication_server.py
+++ Backend/communication_server.py
@@ -1,5 +1,4 @@
 import zmq
-import threading
 import logging
 from .message_handler import MessageHandler
 
@@ -13,43 +12,34 @@ class ServerZeroMQ:
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REP)
         self.socket.bind(bind_address)
-        self.server_thread = None
-
-    def _server_loop(self):
-        """Internal server loop running in a thread."""
-        while self.running:
-            try:
-                message = self.socket.recv_json()
-                logger.info('Message received')
-                response = self.handler.process_message(message=message)
-                self.socket.send_json(response)
-                logger.info('Response sent')
-            except zmq.Again:
-                continue
-            except Exception as e:
-                logger.error('Error in server loop: %s', e)
+        self.poller = zmq.Poller()
+        self.poller.register(self.socket, zmq.POLLIN)
 
     def start(self):
-        """Starts the server in a background thread."""
+        """Main server loop with poller for interruptibility."""
         self.running = True
         logger.info('Server ready and listening')
-        self.server_thread = threading.Thread(target=self._server_loop, daemon=True)
-        self.server_thread.start()
 
-        # Block the main thread until KeyboardInterrupt
         try:
             while self.running:
-                self.server_thread.join(timeout=1)
+                # Poll with timeout to allow KeyboardInterrupt
+                socks = dict(self.poller.poll(timeout=1000))  # 1 sec timeout
+                if socks.get(self.socket) == zmq.POLLIN:
+                    message = self.socket.recv_json()
+                    logger.info('Message received')
+                    response = self.handler.process_message(message=message)
+                    self.socket.send_json(response)
+                    logger.info('Response sent')
         except KeyboardInterrupt:
             logger.info('Keyboard interrupt received')
-
-        self.close()
+        except Exception as e:
+            logger.error('Error in server loop: %s', e)
+        finally:
+            self.close()
 
     def close(self):
         """Clean shutdown."""
         logger.info('Shutting down server')
         self.running = False
-        if self.server_thread and self.server_thread.is_alive():
-            self.server_thread.join(timeout=1)
         self.socket.close()
         self.context.term()
